9d53d3e28d5831b0e07c2bd02ae9f7d7ddc68edc
Triana Cerda <trianaRcerda@gmail.com>
2021-11-29 13:45:24 -0800

    added subtitle state + rendering to page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -9,12 +9,13 @@ import './Home.css'
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor setTitle={setTitle} />
+      <Preview title={title} subtitle={subtitle} />
+      <Editor title={title} setTitle={setTitle} subtitle={subtitle} setSubtitle={setSubtitle} />
     </main>
   )
 }
