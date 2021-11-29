d90d832292faa019879fac46893f780ccbe4fbfa
Triana Cerda <trianaRcerda@gmail.com>
2021-11-29 14:21:41 -0800

    added align state + rendering on page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -11,11 +11,12 @@ export default function Home() {
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
   const [font, setFont] = useState('')
+  const [align, setAlign] = useState()
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} font={font} />
+      <Preview title={title} subtitle={subtitle} font={font} align={align} />
       <Editor
         title={title}
         setTitle={setTitle}
@@ -23,6 +24,8 @@ export default function Home() {
         setSubtitle={setSubtitle}
         font={font}
         setFont={setFont}
+        align={align}
+        setAlign={setAlign}
       />
     </main>
   )
