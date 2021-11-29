cf644417d11b0afd6f469ecf416de72830cfea25
Triana Cerda <trianaRcerda@gmail.com>
2021-11-29 14:19:36 -0800

    added font state + rendering to page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,12 +10,20 @@ export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('')
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} />
-      <Editor title={title} setTitle={setTitle} subtitle={subtitle} setSubtitle={setSubtitle} />
+      <Preview title={title} subtitle={subtitle} font={font} />
+      <Editor
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        font={font}
+        setFont={setFont}
+      />
     </main>
   )
 }
